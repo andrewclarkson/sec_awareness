d662d67eccd58072774ceb301a95b5884a38dd55
Andrew Clarkson <andrewclarkson@live.com>
2014-04-28 16:07:58 -0500

    moar pythonic

--- calc.py
+++ calc.py
@@ -1,14 +1,41 @@
 from collections import OrderedDict
 
-def space(length):
-    return " " * (35 - length)
+class Worker():
+    hourly_wage = 0
+
+    def __init__(self, wage):
+        self.hourly_wage = wage
 
 
-# CISO gets paid $85 / hour
-CISO = 85
+class Task():
+    
+    def __str__(self):
+        line = "{name}:{space}hours:{hours}\tcapex:{capex}\top:{opex}\tsub:{sub}"
+        
+        variables = {
+                "name": self.name, 
+                "space": space(25, len(self.name)),
+                "hours": self.hours,
+                "capex": self.capex,
+                "opex": self.non_capex,
+                "sub": self.subtotal
+        }
+        
+        return line.format(**variables)
+                    
+    def __init__(self, name, who, hours, non_capex=0):
 
-# SecAware team average wage / hour
-SEC_AWARE = 40
+        self.name = name
+        self.hours = hours
+        self.capex = who.hourly_wage * hours
+        self.non_capex = non_capex
+        self.subtotal = self.capex + non_capex
+
+
+
+def space(max, length):
+    return " " * (max - length)
+
 
 ATTENDEES = 50
 
@@ -22,44 +49,46 @@ MISC = 100
 # event cost
 EVENT = FOOD + PRINTING + MISC
 
-# ISAC wage / hour
-ISAC = 40
+sec_aware = Worker(40)
 
-costs = OrderedDict([
-    ("Assign SecAware Team", CISO * 1),
-    ("Review Purchase Materials", 1600),
-    ("Develop Tests", SEC_AWARE * 22),
-    ("Migrate Materials", SEC_AWARE * 4),
-    ("Web Platform", SEC_AWARE * 80),
-    ("Appoint ISAC", CISO * 1),
-    ("Pre tests", ISAC * 0.5),
-    ("Curriculum 1", SEC_AWARE * 40),
-    ("Event 1", SEC_AWARE * 4 + EVENT),
-    ("Curriculum 2", SEC_AWARE * 40),
-    ("Event 2", SEC_AWARE * 4 + EVENT),
-    ("Curriculum 3", SEC_AWARE * 40),
-    ("Event 3", SEC_AWARE * 4 + EVENT),
-    ("Curriculum 4", SEC_AWARE * 40),
-    ("Event 4", SEC_AWARE * 4 + EVENT),
-    ("Post Test", ISAC * 1),
-    ("Feedback", SEC_AWARE * 1),
-    ("Evaluate", SEC_AWARE * 16),
-    ("Report", SEC_AWARE * 8),
-    ("Revise", SEC_AWARE * 40)
-])
+isac = Worker(40)
+
+ciso = Worker(85)
+
+
+tasks = [
+    Task("Assign SecAware Team", ciso, 1),
+    Task("Review Purchase Materials", sec_aware, 40),
+    Task("Develop Tests", sec_aware, 22),
+    Task("Migrate Materials", sec_aware, 4),
+    Task("Web Platform", sec_aware, 80),
+    Task("Appoint ISAC", ciso, 1),
+    Task("Pre tests", isac, 0.5),
+    Task("Curriculum 1", sec_aware, 60),
+    Task("Event 1", sec_aware, 8, EVENT),
+    Task("Curriculum 2", sec_aware, 60),
+    Task("Event 2", sec_aware, 8, EVENT),
+    Task("Curriculum 3", sec_aware, 60),
+    Task("Event 3", sec_aware, 8, EVENT),
+    Task("Curriculum 4", sec_aware, 60),
+    Task("Event 4", sec_aware, 8, EVENT),
+    Task("Post Test", isac, 1),
+    Task("Feedback", sec_aware, 1),
+    Task("Evaluate", sec_aware, 16),
+    Task("Report", sec_aware, 8),
+    Task("Revise", sec_aware, 40)
+]
 
 if __name__ == "__main__":
     
-    print("Per Event:%s$%d" % (space(len("Per Event")), EVENT))
+    print("Per Event:%s$%d" % (space(25, len("Per Event")), EVENT))
     
     total = 0
 
-    for key in costs.keys():
+    for task in tasks:
 
-        subtotal = costs[key]
+        total += task.subtotal
 
-        total += subtotal
+        print(task)
 
-        print("%s:%s$%d" % (key, space(len(key)), subtotal))
-
-    print("Total:%s$%d" % (space(len("Total")), total))
+    print("Total:%s$%d" % (space(25, len("Total")), total))
